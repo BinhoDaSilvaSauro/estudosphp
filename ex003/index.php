30d13aa0b40d4ccebd2c2c6ff513634f2f312384
unknown <rubenslira371@gmail.com>
2023-05-26 12:16:31 -0300

    Teste de tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>&#x1F9A3; Tipos Primitivos</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php
+        $nome = "Rubens";
+        echo "<p>".var_dump($nome)."</p>";
+
+        $idade = 19;
+        echo "<p>".var_dump($idade)."</p>";
+        
+        $peso = 61.2;
+        echo "<p>".var_dump($peso)."</p>";
+        
+        $casado = false;
+        echo "<p>".var_dump($casado)."</p>";
+        ?>
+</body>
+</html>
